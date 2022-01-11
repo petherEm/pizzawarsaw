f8ae2e652beb1ba0469b60daaa48439bcfc7e446
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-01-11 18:13:18 +0100

    update1.2

--- pages/components/PizzaCard.jsx
+++ pages/components/PizzaCard.jsx
@@ -7,7 +7,7 @@ const PizzaCard = () => {
             <Image src="/images/f2.png" width="200" height="200" alt="pizza" />
             <h2 className="text-2xl font-semibold text-green-900 mt-4">Capriciosa</h2>
             <p><span className="font-bold">Price: </span>34 PLN</p>
-            <p className="text-gray-600 italic">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+            <p className="text-gray-600 italic text-center">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
         </div>
     )
 }
--- pages/components/PizzaList.jsx
+++ pages/components/PizzaList.jsx
@@ -5,8 +5,8 @@ import PizzaCard from './PizzaCard'
 const PizzaList = () => {
     return (
         <div className="flex flex-col lg:p-20 md:p-20 items-center">
-            <h1 className="text-4xl uppercase font-bold">🍕🍕Bite Best Pizza in Town🍕🍕</h1>
-            <p className="w-[70%] py-10 text-2xl text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum molestias debitis consequatur, eveniet porro itaque provident, dicta dolores reprehenderit vero fugiat corrupti ab unde aut sapiente labore ipsam tempore quam?
+            <h1 className="lg:text-4xl text-3xl uppercase font-bold p-4">🍕🍕Bite Best Pizza in Town🍕🍕</h1>
+            <p className="lg:w-[70%] w-[90%] text-center py-10 text-2xl text-gray-500">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum molestias debitis consequatur, eveniet porro itaque provident, dicta dolores reprehenderit vero fugiat corrupti ab unde aut sapiente labore ipsam tempore quam?
 
             </p>
             <div className="w-[80%] flex flex-wrap gap-x-4 items-center justify-center">
